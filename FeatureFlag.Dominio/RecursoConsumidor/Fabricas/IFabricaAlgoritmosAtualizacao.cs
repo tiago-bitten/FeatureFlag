fbaf439cdad41cd8e83c689fd8d154ab2328390b
tiago-bitten <tiago.sbittencourt@icloud.com>
2024-11-16 12:06:49 -0300

    add teste de atualizar disponibilidades

--- FeatureFlag.Dominio/RecursoConsumidor/Fabricas/IFabricaAlgoritmosAtualizacao.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FeatureFlag.Dominio.RecursoConsumidor.Fabricas;
-
-public interface IFabricaAlgoritmosAtualizacao
-{
-    void AlgoritmoVitao(string identificadorRecurso);
-    Task AlgoritmoFisherYates(string identificadorRecurso, int quantidadeAlvo);
-}
--- FeatureFlag.Dominio/RecursoConsumidor/ServRecursoConsumidor.cs
+++ FeatureFlag.Dominio/RecursoConsumidor/ServRecursoConsumidor.cs
@@ -1,5 +1,4 @@
 ﻿using FeatureFlag.Domain.Infra;
-using FeatureFlag.Dominio.RecursoConsumidor.Fabricas;
 using FeatureFlag.Shared.Extensions;
 using FeatureFlag.Shared.Helpers;
 
@@ -10,17 +9,14 @@ public class ServRecursoConsumidor : ServBase<RecursoConsumidor, IRepRecursoCons
     #region Ctor
     private readonly IRepRecurso _repRecurso;
     private readonly IRepConsumidor _repConsumidor;
-    private readonly IFabricaAlgoritmosAtualizacao _fabricaAlgoritmosAtualizacao;
     
     public ServRecursoConsumidor(IRepRecursoConsumidor repositorio,
                                  IRepRecurso repRecurso,
-                                 IRepConsumidor repConsumidor, 
-                                 IFabricaAlgoritmosAtualizacao fabricaAlgoritmosAtualizacao) 
+                                 IRepConsumidor repConsumidor) 
         : base(repositorio)
     {
         _repRecurso = repRecurso;
         _repConsumidor = repConsumidor;
-        _fabricaAlgoritmosAtualizacao = fabricaAlgoritmosAtualizacao;
     }
     #endregion
 
@@ -38,9 +34,86 @@ public class ServRecursoConsumidor : ServBase<RecursoConsumidor, IRepRecursoCons
     #endregion
 
     #region AtualizarDisponibilidadesAsync
-    public async Task AtualizarDisponibilidadesAsync(string identificadorRecurso, int quantidadeAlvo)
+    public Task AtualizarDisponibilidadesAsync(string identificadorRecurso, int quantidadeAlvo)
     {
-        await _fabricaAlgoritmosAtualizacao.AlgoritmoFisherYates(identificadorRecurso, quantidadeAlvo);
+        // TODO: SERA ToListAsync
+        var habilitados = Repositorio.RecuperarHabilitadosPorRecurso(identificadorRecurso).ToList();
+        var desabilitados = Repositorio.RecuperarDesabilitadosPorRecurso(identificadorRecurso).ToList();
+
+        var quantidadeRestante = quantidadeAlvo - habilitados.Count;
+
+        switch (quantidadeRestante)
+        {
+            case > 0:
+                HabilitarConsumidores(desabilitados, quantidadeRestante);
+                break;
+            
+            case < 0:
+                DesabilitarConsumidores(habilitados, quantidadeRestante);
+                break;
+        }
+        
+        return Task.CompletedTask;
     }
+    
+    #region HabilitarConsumidores
+    private void HabilitarConsumidores(List<Domain.RecursoConsumidor> desabilitados, int quantidadeRestante)
+    {
+        var consumidoresParaHabilitar = desabilitados.EmbaralharFisherYates()
+            .Take(quantidadeRestante)
+            .ToList();
+
+        foreach (var consumidor in consumidoresParaHabilitar)
+        {
+            consumidor.Habilitar();
+        }
+    }
+    #endregion 
+
+    #region DesabilitarConsumidores
+    private void DesabilitarConsumidores(List<RecursoConsumidor> habilitados, int quantidadeParaDesabilitarNegativo)
+    {
+        var quantidadeParaDesabilitar = NumeroHelper.ValorAbsoluto(quantidadeParaDesabilitarNegativo);
+
+        var consumidoresParaDesabilitar = habilitados.EmbaralharFisherYates()
+            .Take(quantidadeParaDesabilitar)
+            .ToList();
+
+        foreach (var consumidor in consumidoresParaDesabilitar)
+        {
+            consumidor.Desabilitar();
+        }
+    }
+    #endregion
+    
+    #region AlgoritmoVitao
+    private async Task AlgoritmoVitao(string identificadorRecurso)
+    {
+        var porcentagemRecurso = await _repRecurso.RecuperarPorcentagemPorIdentificadorAsync(identificadorRecurso);
+        
+        var consumidores = Repositorio.RecuperarPorRecurso(identificadorRecurso).ToList();
+        var totalConsumidores = consumidores.Count;
+        var habilitados = Repositorio.RecuperarHabilitadosPorRecurso(identificadorRecurso).Count();
+
+        foreach (var consumidor in consumidores)
+        {
+            var percentualAtual = (habilitados / (decimal)totalConsumidores) * 100;
+            
+            if (percentualAtual < porcentagemRecurso)
+            {
+                if (consumidor.Status == EnumStatusRecursoConsumidor.Habilitado) continue;
+                consumidor.Habilitar();
+                habilitados++;
+            }
+            else
+            {
+                if (consumidor.Status == EnumStatusRecursoConsumidor.Desabilitado) continue;
+                consumidor.Desabilitar();
+                habilitados--;
+            }
+        }
+    }
+    #endregion
+
     #endregion
 }
