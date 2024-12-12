cd3a4c232cc8cf2e70cc4fcd8fd953bcc796fa61
tiago.bittencourt <tiago.bittencourt@nextfit.com.br>
2024-12-11 22:48:25 -0300

    controller

---
+++ FeatureFlag.API/Controllers/Infra/ControllerBaseFeatureFlag.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FeatureFlag.API.Controllers.Infra;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ControllerBaseFeatureFlag : ControllerBase
+{
+    private const string MensagemSucesso = "Operação realizada com sucesso.";
+
+    #region Sucesso
+    protected IActionResult Sucesso<T>(T conteudo, string mensagem = MensagemSucesso) where T : class
+    {
+        var resposta = RespostaBase.Sucesso(conteudo, mensagem);
+
+        return Ok(resposta);
+    }
+    
+    protected IActionResult Sucesso(string mensagem = MensagemSucesso)
+    {
+        var resposta = RespostaBase.Sucesso(mensagem);
+
+        return Ok(resposta);
+    }
+    
+    protected IActionResult Sucesso<T>(T conteudo, int total, string mensagem = MensagemSucesso) where T : class
+    {
+        var resposta = RespostaBase.Sucesso(conteudo, total, mensagem);
+
+        return Ok(resposta);
+    }
+    #endregion
+    
+    #region Erro
+    protected IActionResult Erro(string mensagem)
+    {
+        var resposta = RespostaBase.Erro(mensagem);
+
+        return BadRequest(resposta);
+    }
+    #endregion
+}
---
+++ FeatureFlag.API/Controllers/Infra/RespostaBase.cs
@@ -0,0 +1,31 @@
+﻿namespace FeatureFlag.API.Controllers.Infra;
+
+public record RespostaBase<T>(
+    bool Sucesso,
+    T? Conteudo,
+    string? Mensagem,
+    int? Total)
+    where T : class;
+
+static class RespostaBase
+{
+    public static RespostaBase<T> Sucesso<T>(T conteudo, string mensagem) where T : class
+    {
+        return new RespostaBase<T>(Sucesso: true, Conteudo: conteudo, Mensagem: mensagem, Total: null);
+    }
+    
+    public static RespostaBase<dynamic> Sucesso(string mensagem)
+    {
+        return new RespostaBase<dynamic>(Sucesso: true, Conteudo: null, Mensagem: mensagem, Total: null);
+    }
+
+    public static RespostaBase<T> Sucesso<T>(T conteudo, int total, string mensagem) where T : class
+    {
+        return new RespostaBase<T>(Sucesso: true, Conteudo: conteudo, Mensagem: mensagem, Total: total);
+    }
+
+    public static RespostaBase<dynamic> Erro(string mensagem)
+    {
+        return new RespostaBase<dynamic>(Sucesso: false, Conteudo: null, Mensagem: mensagem, Total: null);
+    }
+}
