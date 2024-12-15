e48a708a6c0a2342fc54bd7f3c4098682ba25ca3
tiago.bittencourt <tiago.bittencourt@nextfit.com.br>
2024-12-15 15:09:35 -0300

    documents

---
+++ FeatureFlag.Repositorio/Infra/EntidadeBaseConfig.cs
@@ -0,0 +1,21 @@
+﻿using FeatureFlag.Dominio.Infra;
+using MongoDB.Bson.Serialization;
+
+namespace FeatureFlag.Repositorio.Infra;
+
+public class EntidadeBaseConfig<T> : IdentificadorConfig<T> where T : EntidadeBase
+{
+    public override void Configurar(BsonClassMap<T> builder)
+    {
+        base.Configurar(builder);
+        
+        builder.MapProperty(x => x.DataCriacao)
+            .SetElementName("datacriacao");
+        
+        builder.MapProperty(x => x.DataAlteracao)
+            .SetElementName("dataalteracao");
+        
+        builder.MapProperty(x => x.Inativo)
+            .SetElementName("inativo");
+    }
+}
